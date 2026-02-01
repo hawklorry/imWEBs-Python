6cfcd418ff8903993ab66a85fed1c731e39afa26
Zhiqiang Yu <zhiqiangyu82@gmail.com>
2026-02-01 17:33:39 -0500

    Add tile drain groundwater parameters

--- imWEBs/database/bmp/bmp_19_tile_drain_management.py
+++ imWEBs/database/bmp/bmp_19_tile_drain_management.py
@@ -41,7 +41,10 @@ class TileDrainParameter:
         self.OrgPCoef = 0.0016
         self.PRCTile = 0.75
         self.CNTile = 0.75
-        self.GWT0 = 1000
+        self.gw0 = 1000
+        self.kg = 0.0035
+        self.base_ex = 1.1
+        self.gwmax = 500
 
     @staticmethod 
     def column_types()->dict:
